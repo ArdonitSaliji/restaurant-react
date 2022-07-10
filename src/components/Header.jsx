9f2193b38fc650866db2b0242c90f3896b28e41e
Ardonit Saliji <ardonit.1980@gmail.com>
2022-07-10 10:23:56 +0200

    responsive 2.2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaBars } from 'react-icons/fa'
+import { RiCloseLine } from 'react-icons/ri'
+import '../ulqin.css'
 const Header = (props) => {
+  const showNavbar = () => {}
+  const [sidebar, setSidebar] = useState(false)
+  const showSidebar = () => setSidebar(!sidebar)
   return (
     <div className='hr'>
       <div className='navbar'>
@@ -8,12 +13,21 @@ const Header = (props) => {
           <img src={process.env.PUBLIC_URL + '/images/ulqin1.png'} alt='' />
           <h1>Restaurant</h1>
         </div>
-        <FaBars className='bars' />
-        <div className='navItems'>
-          <a onClick={props.showHome}>Home</a>
-          <a onClick={props.showMenu}>Menu</a>
-          <a onClick={props.showLocation}>Location</a>
-          <a>Contact</a>
+        <FaBars className='bars' onClick={showSidebar} />
+        <div className={sidebar ? 'navItems active' : 'navItems '}>
+          <a href='#1' className='x'>
+            <RiCloseLine className='close active' onClick={() => setSidebar(false)} />
+          </a>
+          <a href='#2' onClick={props.showHome}>
+            Home
+          </a>
+          <a href='#3' onClick={props.showMenu}>
+            Menu
+          </a>
+          <a href='#4' onClick={props.showLocation}>
+            Location
+          </a>
+          <a href='#5'>Contact</a>
         </div>
       </div>
     </div>
